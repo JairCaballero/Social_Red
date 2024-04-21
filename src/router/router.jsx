d9ea3e12cb40bfba567961975008d9692486c780
JairCaballero <dwancaballero@gmail.com>
2024-04-20 21:46:22 -0500

    creando carpetas para componentes login y registro, creando posibles rutas iniciales de la red

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
-import ErrorPage from "../pages/error-page"
-import Home from "../pages/home/Home"
-import LayoutPublic from "../layouts/LayoutPublic"
+import Home from "@/pages/home/Home"
+import ErrorPage from "@/pages/error-page"
+import LayoutPublic from "@/layouts/LayoutPublic"
 
 const router = createBrowserRouter ([
   {
@@ -12,9 +12,32 @@ const router = createBrowserRouter ([
       {
         index: true,
         element: <Home />
-      }
+      },
+      {
+        path: '/perfil-user'
+      },
+      {
+        path: '/perfil-group'
+      },
     ]
-  }
+  },
+  {
+    path: '/user',
+    children: [
+      {
+        path: '/config'
+      },
+      {
+        path: '/friends'
+      },
+      {
+        path: '/groups'
+      },
+      {
+        path: '/config-group/:id'
+      },
+    ]
+  },
 ])
 
 export default router
