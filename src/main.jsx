a391ba44965af335bdcda44e93ddbeea261800e8
JairCaballero <dwancaballero@gmail.com>
2024-04-20 21:37:14 -0500

    organizando importaciones

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import router from './router/router.jsx'
 import './index.css'
+
+import router from './router/router.jsx'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
